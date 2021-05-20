6bf7249e7ce1f96a9932aa781436ca856daba62f
TYom <hovannisyan98@mail.ru>
2021-05-20 23:56:31 +0400

    Added gitignore, and GULP

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,3 +1,19 @@
+function testWebP(callback){
+    var webP = new Image();
+    webP.onload = webP.onerror = function (){
+        callback(webP.height == 2);
+    }
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+testWebP(function (support) {
+    if (support == true) {
+    document.querySelector('body').classList.add('webp');
+    }
+});
+// =================================================================WEBP
+
+
+
 (function () {
   let can1 = document.getElementById('canvas1');
   let can2 = document.getElementById('canvas2');
